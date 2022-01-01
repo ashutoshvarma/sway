ed3d25e998dd023cb918216231620cd5d918afca
Ashutosh Varma <ashutoshvarma11@live.com>
2022-01-02 02:35:36 +0530

    WIP

--- packages/events/api/events.ts
+++ packages/events/api/events.ts
@@ -1,9 +1,17 @@
 import { readdirSync } from 'fs'
+import * as path from 'path'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 // const DETAILS_DIR = path.resolve(__dirname, 'details')
 
-export default (_request: VercelRequest, response: VercelResponse) => {
+export default (request: VercelRequest, response: VercelResponse) => {
   // const files = readdirSync(DETAILS_DIR, { withFileTypes: true })
-  response.json(readdirSync(__dirname))
+  const { p } = request.query
+  response.json({
+    __dirname: readdirSync(__dirname),
+    __parent: readdirSync(path.resolve(__dirname, '..')),
+    __data: readdirSync(path.resolve(__dirname, '..', 'data')),
+    __details: readdirSync(path.resolve(__dirname, '..', 'data/details')),
+    __p: readdirSync(path.resolve(__dirname, '..', p ? (p as string) : '')),
+  })
 }
