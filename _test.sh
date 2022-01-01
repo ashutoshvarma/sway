542bcbdeea4de3f23f27a465b118231b3a3db779
Ashutosh Varma <ashutoshvarma11@live.com>
2022-01-01 22:10:46 +0530

    [WIP]: test vercel serverless functions files

---
+++ _test.sh
@@ -0,0 +1,22 @@
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 12 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 12 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 12 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 12 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 12 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 12 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 12 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 12 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+
+
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 4 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 4 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 7 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 7 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 7 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 8 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 8 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 8 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+HARDHAT_NETWORK=alfajores yarn hardhat event:mint --event 8 --to 0x2Db47F2e65D92860bf3Dc4b1f1190184109D04A0
+
