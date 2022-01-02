92bc618e73c80b8c62fabc41c5f0961f7185b756
Ashutosh Varma <ashutoshvarma11@live.com>
2022-01-02 18:33:17 +0530

    feat(events): add `/api/events` for getting all events metadat

--- packages/events/api/events.ts
+++ packages/events/api/events.ts
@@ -1,17 +1,21 @@
-import { readdirSync } from 'fs'
+import * as fs from 'fs'
 import * as path from 'path'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { NFTMetadata } from '../src/events'
 
-// const DETAILS_DIR = path.resolve(__dirname, 'details')
-
-export default (request: VercelRequest, response: VercelResponse) => {
-  // const files = readdirSync(DETAILS_DIR, { withFileTypes: true })
-  const { p } = request.query
-  response.json({
-    __dirname: readdirSync(__dirname),
-    __parent: readdirSync(path.resolve(__dirname, '..')),
-    __data: readdirSync(path.resolve(__dirname, '..', 'data')),
-    __details: readdirSync(path.resolve(__dirname, '..', 'data/details')),
-    __p: readdirSync(path.resolve(__dirname, '..', p ? (p as string) : '')),
+const METADATA_DIR = path.resolve(__dirname, '..', 'data/metadata')
+const EVENTS_METADATA = fs
+  .readdirSync(METADATA_DIR, { withFileTypes: true })
+  .map((f) => {
+    if (f.isFile() && f.name.includes('json')) {
+      return <NFTMetadata>JSON.parse(
+        fs.readFileSync(path.join(METADATA_DIR, f.name), {
+          encoding: 'utf8',
+        }),
+      )
+    }
   })
+
+export default async (_request: VercelRequest, response: VercelResponse) => {
+  response.json(EVENTS_METADATA)
 }
