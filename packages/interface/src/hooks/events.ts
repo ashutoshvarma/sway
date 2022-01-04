297b00ea7ee52fd4ea1a3053b7252bc53d0d2d09
Ashutosh Varma <ashutoshvarma11@live.com>
2022-01-04 22:57:58 +0530

    feat: add hooks for `claim` event

--- packages/interface/src/hooks/events.ts
+++ packages/interface/src/hooks/events.ts
@@ -1,35 +1,109 @@
-// import {
-//   useContractKit,
-//   useGetConnectedSigner,
-//   useProvider,
-// } from '@celo-tools/use-contractkit'
-// import { useMemo } from 'react'
-// import { Contract } from '@ethersproject/contracts'
-// import SwayDropDeployment from '@sway/contracts/deployments/alfajores/SwayDrop.json'
-// import { SwayDrop } from '@sway/contracts/typechain'
-// import api from '../utils/api'
+import {
+  useContractKit,
+  useGetConnectedSigner,
+  useProvider,
+} from '@celo-tools/use-contractkit'
+import { SwayDropParticipants } from 'packages/events/src/events'
+import { getMerkleProof } from '@sway/contracts/utils/merkle'
+import { useEffect, useMemo, useState } from 'react'
+import { getSwayDropContract, indexInParticipants } from '../utils/helpers'
 
-// function useIsEligibleForDrop(eventId: string): boolean {
-//   const { address: account, network } = useContractKit()
-//   const library = useProvider()
-//   //   const chainId = network.chainId
+export enum ClaimStatus {
+  // already claimed
+  CLAIMED,
+  // can claim
+  AVAIL,
+  // cannot claim as not in list
+  NOT_AVAIL,
+}
 
-//   return useMemo(() => {
-//     if (!account) return false
-//     const participants = await api.getEventMerkleDetails(eventId)
-//     if(participants.)
+export function useClaimStatus(
+  eventId: string,
+  participants?: SwayDropParticipants,
+): [ClaimStatus, boolean] {
+  const { network, address: account } = useContractKit()
+  const chainId = network.chainId
+  const library = useProvider()
+  const [status, setStatus] = useState<ClaimStatus>(ClaimStatus.NOT_AVAIL)
+  const [loading, setLoading] = useState<boolean>(true)
 
-//     const swayDrop = new Contract(
-//       account,
-//       SwayDropDeployment.abi,
-//       library,
-//     ) as SwayDrop
+  useEffect(() => {
+    try {
+      setLoading(true)
+      if (account && participants) {
+        // check if account in drop list
+        if (indexInParticipants(account, participants) !== -1) {
+          // get if already claimed
+          getSwayDropContract(library)
+            .claimed(eventId, account)
+            .then((c) =>
+              setStatus(c === true ? ClaimStatus.CLAIMED : ClaimStatus.AVAIL),
+            )
+        } else {
+          setStatus(ClaimStatus.NOT_AVAIL)
+        }
+      }
+    } catch (error) {
+      console.error(error, { account, eventId })
+    } finally {
+      setLoading(false)
+    }
+  }, [eventId, chainId, library])
 
-    
+  return [status, loading]
+}
 
-//     return true
-//   }, [account, network, eventId])
-// }
+export function useClaimCallback(
+  eventId: string,
+  participants?: SwayDropParticipants,
+): {
+  callback: null | (() => Promise<string>)
+  error: string | null
+} {
+  const { network, address: account } = useContractKit()
+  const chainId = network.chainId
+  const library = useProvider()
+  const getConnectedSigner = useGetConnectedSigner()
 
+  return useMemo(() => {
+    if (!library || !account || !chainId || !participants) {
+      return {
+        callback: null,
+        error: 'Missing dependencies',
+      }
+    }
 
-export default 1
+    return {
+      callback: async function onSwap(): Promise<string> {
+        try {
+          const signer = await getConnectedSigner()
+          const swayDrop = getSwayDropContract(library, account).connect(signer)
+          const index = indexInParticipants(account, participants)
+          const proof = getMerkleProof(
+            index,
+            eventId,
+            participants.participants,
+          )
+          const hash = (
+            await (await swayDrop.claim(index, eventId, account, proof)).wait()
+          ).transactionHash
+          return hash
+        } catch (error: any) {
+          // if the user rejected the tx, pass this along
+          if (error?.code === 4001) {
+            throw new Error('Transaction rejected.')
+          } else {
+            // otherwise, the error was unexpected and we need to convey that
+            console.error(`Claim failed`, error, {
+              eventId,
+              account,
+              participants,
+            })
+            throw new Error(`Claim failed: ${error.message}`)
+          }
+        }
+      },
+      error: null,
+    }
+  }, [eventId, chainId, library, getConnectedSigner, participants])
+}
