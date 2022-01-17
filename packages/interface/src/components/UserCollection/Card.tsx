fcf12eba860cd9f51c5f781ba7ce8854f9e2404a
Ashutosh Varma <ashutoshvarma11@live.com>
2022-01-17 12:10:17 +0530

    fix: use `trauncate()`  for transcation hash

--- packages/interface/src/components/UserCollection/Card.tsx
+++ packages/interface/src/components/UserCollection/Card.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react'
 import styles from './Card.module.css'
-import {Token } from './Collections';
-import {shortenAddress} from '../../utils/helpers';
+import { Token } from './Collections'
+import { truncate } from '../../utils/helpers'
 
 import cardImg from '../../assets/illustrations/hero-illus.svg'
 import ig from '../../assets/icons/ig.svg'
@@ -10,17 +10,16 @@ import pinterest from '../../assets/icons/pinterest.svg'
 import twitter from '../../assets/icons/twitter.svg'
 
 interface Props {
-  loading?: boolean,
+  loading?: boolean
   token?: Token
 }
 
-
-function Card({loading,token}:Props): ReactElement {
+function Card({ loading, token }: Props): ReactElement {
   let classes = [styles['Card']]
-  if(loading) classes.push(styles['Loading'])
+  if (loading) classes.push(styles['Loading'])
 
   return (
-    <div className={classes.join(" ")}>
+    <div className={classes.join(' ')}>
       <div className={styles['CardThumbContainer']}>
         <img src={cardImg} className={styles['CardThumb']} alt="thumbnail" />
       </div>
@@ -28,7 +27,11 @@ function Card({loading,token}:Props): ReactElement {
         <span className={styles['Head']}>Status</span>
         <span className={styles['Head']}>Transaction ID</span>
         <span className={styles['Completed']}>Completed</span>
-        <span className={styles['Hash']}>{token?.transactionHash ? shortenAddress(token.transactionHash, 100) :"0iyjh8389dh..3iresw12"}</span>
+        <span className={styles['Hash']}>
+          {token?.transactionHash
+            ? truncate(token.transactionHash, 100)
+            : '0iyjh8389dh..3iresw12'}
+        </span>
       </div>
       <h4 className={styles['Title']}>Collect Best NFT’s Quickly!</h4>
       <div className={styles['Share']}>
